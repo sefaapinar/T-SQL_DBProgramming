66150b95aee3c41b51c0a8e0bf744c080dc62b8d
Sefa Pınar <77726883+sefaapinar@users.noreply.github.com>
2023-03-19 22:27:27 +0300

    GoExams
    GoExams

---
+++ Loops/GoExam.sql
@@ -0,0 +1,11 @@
+
+
+CREATE TABLE DATES (ID INT IDENTITY(1,1),DATE_ DATETIME)
+
+
+SELECT * FROM DATES
+
+INSERT INTO DATES (DATE_) VALUES(GETDATE())
+GO 5 --GO Komutu kendinin üstündeki tüm satýrlarý çalýþtýrýr. GO komutu döngü komutudur.
+
+
